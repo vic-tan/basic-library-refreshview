983bcb7b0354f683aa254cc499e07398cdb60fdf
tanlifei <tanlifei@zhixueyun.com>
2016-12-27 16:33:42 +0800

    update

---
+++ library/src/main/java/com/tlf/basic/refreshview/more/OnLoadMoreListener.java
@@ -0,0 +1,10 @@
+package com.tlf.basic.refreshview.more;
+
+/**
+ * Desction:
+ * Author:pengjianbo
+ * Date:16/3/1 下午8:36
+ */
+public interface OnLoadMoreListener {
+    void loadMore();
+}
---
+++ library/src/main/java/com/tlf/basic/refreshview/more/OnScrollBottomListener.java
@@ -0,0 +1,10 @@
+package com.tlf.basic.refreshview.more;
+
+/**
+ * Desction:
+ * Author:pengjianbo
+ * Date:16/3/1 下午7:03
+ */
+interface OnScrollBottomListener {
+    void onScorllBootom();
+}

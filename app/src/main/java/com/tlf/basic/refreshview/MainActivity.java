b6d9af0bf4ead1a06042b6515f38a34dd4d25aff
tanlifei <tanlifei@zhixueyun.com>
2016-12-27 16:49:36 +0800

    update

--- app/src/main/java/com/tlf/basic/refreshview/MainActivity.java
+++ app/src/main/java/com/tlf/basic/refreshview/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String one ="./gradlew clean build bintrayUpload -PbintrayUser=vic-tan -PbintrayKey=8970dbd317d720d9b1176877f90c9bd6a431e650 -PdryRun=false";
+
     }
 }
